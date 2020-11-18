8235a7739de8c5246db85747b43df32e57c0d563
Valdas <valdas@e-servisas.lt>
2020-11-18 17:52:59 +0200

    lietuviskas rikiavimas

--- src/d17/Mokinys.java
+++ src/d17/Mokinys.java
@@ -1,5 +1,22 @@
 package d17;
 
+/*
+
+      Object -> Zmogus -> Mokinys -> Studentas
+
+
+      Zmogus z = new Mokinys();
+      z.pavarde = "Xxx";
+      z.klase = 3; !!!
+      Mokinys m = (Mokinys)z;
+      m.klase = 3; // ((Mokinys)z).klase = 3;
+
+      Zmogus z2 = new Studentas("Jonas", "Marija");
+
+      Object o = z;
+
+ */
+
 public class Mokinys extends Zmogus {
     public int klase;
     public int[] trimestras;
@@ -16,5 +33,11 @@ public class Mokinys extends Zmogus {
     Mokinys(String vardas, String pavarde) {
         this.vardas = vardas;
         this.pavarde = pavarde;
+    }
+}
+
+class Studentas extends Mokinys {
+    public Studentas(String vardas, String pavarde) {
+        super(vardas, pavarde);
     }
 }
