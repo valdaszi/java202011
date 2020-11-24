5ec1857f928dd7ffa9f45b58db8f1f4139b3dac6
Valdas <valdas@e-servisas.lt>
2020-11-24 11:17:16 +0200

    wrapperiai

--- src/d23/pvz/C.java
+++ src/d23/pvz/C.java
@@ -14,6 +14,26 @@ public class C implements IA, IB {
     public void printName() {
         System.out.println("C");
     }
+
+    public void metodasKurisNiekoNegrazina() {
+        System.out.println("Cia metodasKurisNiekoNegrazina");
+        //
+        if (Math.random() < 0.5) return;
+        //
+        //
+        System.out.println("O cia metodasKurisNiekoNegrazina pabaiga");
+        return;
+    }
+
+    public int metodasKurisGrazinaIntegeri() {
+        System.out.println("Cia metodasKurisGrazinaIntegeri");
+        //
+        if (Math.random() < 0.5) return 1;
+        //
+        //
+        System.out.println("O cia metodasKurisGrazinaIntegeri pabaiga");
+        return 2;
+    }
 }
 
 
@@ -22,5 +42,12 @@ class Main {
 
         C c = new C();
         c.printName();
+
+        c.metodasKurisNiekoNegrazina();
+
+        c.metodasKurisGrazinaIntegeri();
+        int x = c.metodasKurisGrazinaIntegeri();
+
+        System.out.println("Pabaiga");
     }
 }
